23aedabc3edc387a9f3959da40e85cd8f0dfe338
andreasZel <antreaszel@gmail.com>
2026-05-23 03:17:07 +0300

    removed unused ui

--- ai-application/app/(tabs)/index.tsx
+++
@@ -1,6 +0,0 @@
-import { Image } from 'expo-image';
-import { StaticObjectScanner } from '@/components/StaticObjectScanner';
-
-export default function HomeScreen() {
-  return (<StaticObjectScanner />);
-}
